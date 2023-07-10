46c29cc245f55d377921ab9714f6c289f46bee50
shunze.chen <qianlonwork@outlook.com>
2023-07-10 16:58:17 +0800

    migration from older computer

--- axios/lib/adapters/xhr.js
+++ axios/lib/adapters/xhr.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-var utils = require('./../utils');
-var settle = require('./../core/settle');
-var buildURL = require('./../helpers/buildURL');
-var buildFullPath = require('../core/buildFullPath');
-var parseHeaders = require('./../helpers/parseHeaders');
-var isURLSameOrigin = require('./../helpers/isURLSameOrigin');
-var createError = require('../core/createError');
+var utils = require("./../utils");
+var settle = require("./../core/settle");
+var buildURL = require("./../helpers/buildURL");
+var buildFullPath = require("../core/buildFullPath");
+var parseHeaders = require("./../helpers/parseHeaders");
+var isURLSameOrigin = require("./../helpers/isURLSameOrigin");
+var createError = require("../core/createError");
 
 module.exports = function xhrAdapter(config) {
   return new Promise(function dispatchXhrRequest(resolve, reject) {
@@ -14,20 +14,24 @@ module.exports = function xhrAdapter(config) {
     var requestHeaders = config.headers;
 
     if (utils.isFormData(requestData)) {
-      delete requestHeaders['Content-Type']; // Let the browser set it
+      delete requestHeaders["Content-Type"]; // Let the browser set it
     }
 
     var request = new XMLHttpRequest();
 
     // HTTP basic authentication
     if (config.auth) {
-      var username = config.auth.username || '';
-      var password = config.auth.password || '';
-      requestHeaders.Authorization = 'Basic ' + btoa(username + ':' + password);
+      var username = config.auth.username || "";
+      var password = config.auth.password || "";
+      requestHeaders.Authorization = "Basic " + btoa(username + ":" + password);
     }
 
     var fullPath = buildFullPath(config.baseURL, config.url);
-    request.open(config.method.toUpperCase(), buildURL(fullPath, config.params, config.paramsSerializer), true);
+    request.open(
+      config.method.toUpperCase(),
+      buildURL(fullPath, config.params, config.paramsSerializer),
+      true
+    );
 
     // Set the request timeout in MS
     request.timeout = config.timeout;
@@ -42,20 +46,30 @@ module.exports = function xhrAdapter(config) {
       // handled by onerror instead
       // With one exception: request that using file: protocol, most browsers
       // will return status as 0 even though it's a successful request
-      if (request.status === 0 && !(request.responseURL && request.responseURL.indexOf('file:') === 0)) {
+      // 会在 onerror 中被捕获
+      if (
+        request.status === 0 &&
+        !(request.responseURL && request.responseURL.indexOf("file:") === 0)
+      ) {
         return;
       }
 
       // Prepare the response
-      var responseHeaders = 'getAllResponseHeaders' in request ? parseHeaders(request.getAllResponseHeaders()) : null;
-      var responseData = !config.responseType || config.responseType === 'text' ? request.responseText : request.response;
+      var responseHeaders =
+        "getAllResponseHeaders" in request
+          ? parseHeaders(request.getAllResponseHeaders())
+          : null;
+      var responseData =
+        !config.responseType || config.responseType === "text"
+          ? request.responseText
+          : request.response;
       var response = {
         data: responseData,
         status: request.status,
         statusText: request.statusText,
         headers: responseHeaders,
         config: config,
-        request: request
+        request: request,
       };
 
       settle(resolve, reject, response);
@@ -70,7 +84,7 @@ module.exports = function xhrAdapter(config) {
         return;
       }
 
-      reject(createError('Request aborted', config, 'ECONNABORTED', request));
+      reject(createError("Request aborted", config, "ECONNABORTED", request));
 
       // Clean up request
       request = null;
@@ -80,7 +94,7 @@ module.exports = function xhrAdapter(config) {
     request.onerror = function handleError() {
       // Real errors are hidden from us by the browser
       // onerror should only fire if it's a network error
-      reject(createError('Network Error', config, null, request));
+      reject(createError("Network Error", config, null, request));
 
       // Clean up request
       request = null;
@@ -88,12 +102,11 @@ module.exports = function xhrAdapter(config) {
 
     // Handle timeout
     request.ontimeout = function handleTimeout() {
-      var timeoutErrorMessage = 'timeout of ' + config.timeout + 'ms exceeded';
+      var timeoutErrorMessage = "timeout of " + config.timeout + "ms exceeded";
       if (config.timeoutErrorMessage) {
         timeoutErrorMessage = config.timeoutErrorMessage;
       }
-      reject(createError(timeoutErrorMessage, config, 'ECONNABORTED',
-        request));
+      reject(createError(timeoutErrorMessage, config, "ECONNABORTED", request));
 
       // Clean up request
       request = null;
@@ -103,12 +116,14 @@ module.exports = function xhrAdapter(config) {
     // This is only done if running in a standard browser environment.
     // Specifically not if we're in a web worker, or react-native.
     if (utils.isStandardBrowserEnv()) {
-      var cookies = require('./../helpers/cookies');
+      var cookies = require("./../helpers/cookies");
 
       // Add xsrf header
-      var xsrfValue = (config.withCredentials || isURLSameOrigin(fullPath)) && config.xsrfCookieName ?
-        cookies.read(config.xsrfCookieName) :
-        undefined;
+      var xsrfValue =
+        (config.withCredentials || isURLSameOrigin(fullPath)) &&
+        config.xsrfCookieName
+          ? cookies.read(config.xsrfCookieName)
+          : undefined;
 
       if (xsrfValue) {
         requestHeaders[config.xsrfHeaderName] = xsrfValue;
@@ -116,9 +131,12 @@ module.exports = function xhrAdapter(config) {
     }
 
     // Add headers to the request
-    if ('setRequestHeader' in request) {
+    if ("setRequestHeader" in request) {
       utils.forEach(requestHeaders, function setRequestHeader(val, key) {
-        if (typeof requestData === 'undefined' && key.toLowerCase() === 'content-type') {
+        if (
+          typeof requestData === "undefined" &&
+          key.toLowerCase() === "content-type"
+        ) {
           // Remove Content-Type if data is undefined
           delete requestHeaders[key];
         } else {
@@ -140,20 +158,20 @@ module.exports = function xhrAdapter(config) {
       } catch (e) {
         // Expected DOMException thrown by browsers not compatible XMLHttpRequest Level 2.
         // But, this can be suppressed for 'json' type as it can be parsed by default 'transformResponse' function.
-        if (config.responseType !== 'json') {
+        if (config.responseType !== "json") {
           throw e;
         }
       }
     }
 
     // Handle progress if needed
-    if (typeof config.onDownloadProgress === 'function') {
-      request.addEventListener('progress', config.onDownloadProgress);
+    if (typeof config.onDownloadProgress === "function") {
+      request.addEventListener("progress", config.onDownloadProgress);
     }
 
     // Not all browsers support upload events
-    if (typeof config.onUploadProgress === 'function' && request.upload) {
-      request.upload.addEventListener('progress', config.onUploadProgress);
+    if (typeof config.onUploadProgress === "function" && request.upload) {
+      request.upload.addEventListener("progress", config.onUploadProgress);
     }
 
     if (config.cancelToken) {
